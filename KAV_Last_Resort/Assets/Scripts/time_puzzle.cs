e62c2d367323f53a8a35088227ea086dc5a255ff
kav909 <kavinrajm2008@gmail.com>
2025-10-22 18:34:05 -0500

    s

---
+++ KAV_Last_Resort/Assets/Scripts/time_puzzle.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class time_puzzle_worldX_fixed : MonoBehaviour
+{
+    [SerializeField] GameObject bar;
+    [SerializeField] GameObject circle;
+    [SerializeField] GameObject mark;
+
+    [SerializeField] Transform left;
+    [SerializeField] Transform right;
+
+    [SerializeField] float speed = 2f;
+
+    private int direction = 1;
+ 
+    
+
+    
+    void Start()
+    {
+        
+       
+    }
+    //have to use y axis because the bar is rotated 90 degrees causing x and y to be swapped
+    void Update()
+    {
+        Vector2 target = currentTarget();
+        circle.transform.position = Vector3.Lerp(circle.transform.position, target,speed* Time.deltaTime);
+
+        float distance = (target - (Vector2)circle.transform.position).magnitude;
+        if (distance < 0.1f)
+        {
+            direction *= -1;
+        }
+
+    }
+
+    private Vector2 currentTarget()
+    {
+        if (direction == 1)
+        {
+            return right.position;
+        }
+        else
+        {
+            return left.position;
+        }
+        /*if (  circle.transform.position.y >= right.position.y&& movingRight)
+        {
+            circle.transform.position = Vector3.SmoothDamp(circle.transform.position, left.position, ref velocity, .2f );
+            movingRight = false;
+            Debug.Log("Right reached");
+        }
+        else if ( circle.transform.position.y <= left.position.y&& !movingRight )
+        {
+            movingRight = true;
+            Debug.Log("Left reached");
+            circle.transform.position = Vector3.SmoothDamp(circle.transform.position, right.position, ref velocity, .2f);
+        }*/
+    }
+}
