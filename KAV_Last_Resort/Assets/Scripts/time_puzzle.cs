80e38a1966c65e69991d8b6c40274d5d5a71389a
kav909 <kavinrajm2008@gmail.com>
2025-10-23 17:42:46 -0500

    Update time_puzzle.cs

--- KAV_Last_Resort/Assets/Scripts/time_puzzle.cs
+++ KAV_Last_Resort/Assets/Scripts/time_puzzle.cs
@@ -6,25 +6,36 @@ public class time_puzzle_worldX_fixed : MonoBehaviour
     [SerializeField] GameObject bar;
     [SerializeField] GameObject circle;
     [SerializeField] GameObject mark;
+    [SerializeField] GameObject test;
 
     [SerializeField] Transform left;
     [SerializeField] Transform right;
+    
 
     [SerializeField] float speed = 2f;
 
     private int direction = 1;
- 
-    
 
-    
+
+    private float waitTime = 200f;
+    private float timeWaited = 0f;
+
+
     void Start()
     {
         
-       
     }
     //have to use y axis because the bar is rotated 90 degrees causing x and y to be swapped
     void Update()
     {
+        timeWaited += Time.deltaTime * 1000f;
+        if (Input.GetKey(KeyCode.Space) && timeWaited > waitTime) 
+        {
+            chcekHit();
+            
+
+            timeWaited = 0f;
+        }
         Vector2 target = currentTarget();
         circle.transform.position = Vector3.Lerp(circle.transform.position, target,speed* Time.deltaTime);
 
@@ -58,5 +69,30 @@ public class time_puzzle_worldX_fixed : MonoBehaviour
             Debug.Log("Left reached");
             circle.transform.position = Vector3.SmoothDamp(circle.transform.position, right.position, ref velocity, .2f);
         }*/
+    }
+
+    private void markMove()
+    {
+        float randomNumber = Random.Range(left.position.x, right.position.x);
+        
+        mark.transform.position = new Vector2(randomNumber, mark.transform.position.y);
+        Debug.Log("Mark pos: " + randomNumber);
+
+    }
+
+    private void chcekHit() 
+    {
+        float markPos= mark.transform.position.x;
+        float circlePos= circle.transform.position.x;
+        float distance = Mathf.Abs(markPos - circlePos);
+        if (distance <= circle.GetComponent<SpriteRenderer>().bounds.size.x )
+        {
+            Debug.Log("Hit!");
+            markMove();
+        }
+        else
+        {
+            Debug.Log("Missed!");
+        }
     }
 }
