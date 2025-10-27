24ce2815813b6e83682104201e965082c9c52a66
kav909 <kavinrajm2008@gmail.com>
2025-10-27 16:18:06 -0500

    s

--- KAV_Last_Resort/Assets/Scripts/mob_controller.cs
+++ KAV_Last_Resort/Assets/Scripts/mob_controller.cs
@@ -2,15 +2,61 @@ using UnityEngine;
 
 public class mob_controller : MonoBehaviour
 {
+    [SerializeField] mob_patrol patrol;
+    [SerializeField] mob_player_track track;
+    [SerializeField] mob_wander wander;
+
+    public enum MobState
+    {
+        Default,
+        Idle,
+        Patrol,
+        Wander,
+        Track,
+
+    }
+
+    public MobState currentState = MobState.Patrol;
+    private MobState defaultState;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        defaultState = currentState;
+        SwitchState(currentState);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwitchState(MobState newState)
+    {
+        currentState = newState;
+        patrol.enabled = newState == MobState.Patrol;
+        track.enabled = newState == MobState.Track;
+        wander.enabled = newState == MobState.Wander;
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SwitchState(MobState.Wander);
+                
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SwitchState(defaultState);
+                
+        }
     }
 }
